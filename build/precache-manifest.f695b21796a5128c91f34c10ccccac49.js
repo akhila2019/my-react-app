1493a2cd78e791a6904ef9df43e0be5a9dcd5ee9
root <root@ip-172-31-49-37.ec2.internal>
2019-03-13 04:32:41 +0000

    first commit

---
+++ build/precache-manifest.f695b21796a5128c91f34c10ccccac49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "6459254d0a9ea104a1a1",
+    "url": "/my-react-app/static/js/main.9d67c987.chunk.js"
+  },
+  {
+    "revision": "3ceef360e3a1c238938e",
+    "url": "/my-react-app/static/js/2.d13e8a28.chunk.js"
+  },
+  {
+    "revision": "6459254d0a9ea104a1a1",
+    "url": "/my-react-app/static/css/main.b13bb4ce.chunk.css"
+  },
+  {
+    "revision": "943e533b13e74b9cc776894fd79835f1",
+    "url": "/my-react-app/index.html"
+  }
+];
